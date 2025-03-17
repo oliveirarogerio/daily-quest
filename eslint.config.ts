221aa6f319c0bac1976c5500c2e95e371a48afde
oliveirarogerio <oliveirarogerio35@gmail.com>
2025-03-17 17:09:26 -0400

    chore: update ESLint and TypeScript dependencies, refine ESLint configuration for better type support

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,13 +2,14 @@ import pluginVue from 'eslint-plugin-vue'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import type { Linter } from '@typescript-eslint/utils'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
 // configureVueProject({ scriptLangs: ['ts', 'tsx'] })
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
-const config = defineConfigWithVueTs(
+const config: Linter.Config = defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
