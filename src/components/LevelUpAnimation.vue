3ca42ed0369c9b108e80921d63eec96e7c0293d5
oliveirarogerio <oliveirarogerio35@gmail.com>
2025-03-17 16:57:49 -0400

    chore: update TypeScript configuration and enhance component definitions

--- src/components/LevelUpAnimation.vue
+++ src/components/LevelUpAnimation.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-interface Props {
+defineProps<{
   show: boolean;
-}
+}>();
 </script>
 
 <template>
--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-interface Props {
-  message: string;
+defineProps<{
   show: boolean;
-}
+  message: string;
+}>();
 </script>
 
 <template>
