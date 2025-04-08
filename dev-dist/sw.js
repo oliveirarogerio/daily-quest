875025abf39ba890103abcda8b917b1a199193bb
Ygor Azambuja <ygorazambuja@gmail.com>
2025-04-08 00:06:31 -0400

    fix(sw): atualizar revisão do arquivo index.html

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-61473718'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5hhl9gvhgo8"
+    "revision": "0.h62b8i2tkg8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
